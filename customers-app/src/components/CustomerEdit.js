ec60957fb886887c04bf8630eb51d6af343e04a5
Armando Castañeda Tenango <armando_ct83@hotmail.com>
2020-05-05 16:41:40 -0500

    validations at local mode in a Field of ReduxForm

--- customers-app/src/components/CustomerEdit.js
+++ customers-app/src/components/CustomerEdit.js
@@ -3,24 +3,38 @@ import PropTypes from 'prop-types';
 import {reduxForm,Field} from 'redux-form'; 
 import {connect} from 'react-redux';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
+//validaciones a nivel local
+const MyField = ({type,input,meta,label,name})=>(
+    <div>
+        <label htmlFor={name}>{label} :</label>
+        <input {...input} type={!type ? "text":type}></input>
+        {
+            meta.touched && meta.error && <span>{meta.error}</span>
+        }
+    </div>
+);
+
+const isNumber = value =>(
+    isNaN(Number(value)) && "El campo debe ser un numero"
+);
+
+const isRequired = value => (
+    !value && "Este campo es requerido"
+);
 
 const CustomerEdit = ({name,dni,age}) => {
     return (
         <div>
             <h2>Edicion del cliente</h2>
             <form action="">
-                <div>
-                    <label htmlFor="name">Nombre :</label>
-                    <Field name="name" component="input" type="text"></Field>
-                </div>
-                <div>
-                    <label htmlFor="dni">Dni: </label>
-                    <Field name="dni" component="input" type="text"></Field>
-                </div>
-                <div>
-                    <label htmlFor="age">Edad :</label>
-                    <Field name="age" component="input" type="number"></Field>
-                </div>
+                    <Field name="name" component={MyField} type="text"
+                            validate={isRequired} label="Nombre"></Field>
+
+                    <Field name="dni" component={MyField} type="text"
+                            validate={[isRequired,isNumber]} label="Dni"></Field>
+
+                    <Field name="age" component={MyField} type="number" 
+                    validate={isNumber} label="Edad"></Field>
             </form>
         </div>
     );
