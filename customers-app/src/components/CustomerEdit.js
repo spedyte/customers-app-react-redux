c965bf70b8d08e359e744e305781913390bd05b0
Armando Castañeda Tenango <armando_ct83@hotmail.com>
2020-05-05 16:03:05 -0500

    added Redux Form & passing parameters

--- customers-app/src/components/CustomerEdit.js
+++ customers-app/src/components/CustomerEdit.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {reduxForm,Field} from 'redux-form'; 
+import {connect} from 'react-redux';
 
 const CustomerEdit = ({name,dni,age}) => {
     return (
         <div>
             <h2>Edicion del cliente</h2>
-            <h3>Nombre: {name} / Dni: {dni} / Edad: {age}</h3>
+            <form action="">
+                <div>
+                    <label htmlFor="name">Nombre :</label>
+                    <Field name="name" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="dni">Dni: </label>
+                    <Field name="dni" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Edad :</label>
+                    <Field name="age" component="input" type="number"></Field>
+                </div>
+            </form>
         </div>
     );
 };
@@ -16,4 +31,8 @@ CustomerEdit.propTypes = {
     age:PropTypes.number,
 };
 
-export default CustomerEdit;
+const mapStateToProps=(state,props)=>({
+    initialValues:props
+})
+
+export default connect(mapStateToProps,null)(reduxForm({form:'CustomerEdit'})(CustomerEdit));
--- customers-app/src/reducers/index.js
+++ customers-app/src/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
-
+import {reducer as reduxForm} from 'redux-form';
 import {customers} from './customers';
 
 export default combineReducers({
     customers,
+    form:reduxForm
 });
