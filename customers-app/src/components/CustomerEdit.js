71c447b3aa744475904eaac09d860086688c6baf
Armando Castañeda Tenango <armando_ct83@hotmail.com>
2020-05-07 13:18:05 -0500

    CustomerEdit transform to Uncontroled component to set foucs in first textbox

--- customers-app/src/components/CustomerEdit.js
+++ customers-app/src/components/CustomerEdit.js
@@ -1,21 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {reduxForm,Field, isSubmitting, submitSucceeded,pristine} from 'redux-form'; 
-import {connect} from 'react-redux';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 import CustomersActions from './../components/CustomersActions';
 import { Prompt } from 'react-router-dom';
-
-//validaciones a nivel local
-const MyField = ({type,input,meta,label,name})=>(
-    <div>
-        <label htmlFor={name}>{label} :</label>
-        <input {...input} type={!type ? "text":type}></input>
-        {
-            meta.touched && meta.error && <span>{meta.error}</span>
-        }
-    </div>
-);
 
 //Estas validaciones a nivel de Field tiene prioridad sobre las validaciones a nivel global
 const isNumber = value =>(
@@ -45,31 +33,56 @@ const toCamelCaseM = value => value && value.toLowerCase();
 const onlyGrow = (value,previousValue,values) => 
     value && (!previousValue?value: (value>previousValue? value:previousValue));
 
-const CustomerEdit = ({name,dni,age,handleSubmit,submitting,onBack,pristine,submitSucceeded}) => {
-    return (
+class CustomerEdit extends Component {
+    componentDidMount() {
+        if(this.txt)
+        {
+            this.txt.focus();
+        }
+    }
+    
+    //validaciones a nivel local
+    renderField = ({type,input,meta,label,name,withFocus})=>(
         <div>
-            <h2>Edicion del cliente</h2>
-            <form onSubmit={handleSubmit}>
-                    <Field name="name" component={MyField} type="text"
-                             label="Nombre" parse={toUpper} format={toCamelCaseM}></Field>
-
-                    <Field name="dni" component={MyField} type="text"
-                            validate={[isNumber]} label="Dni"></Field>
-
-                    <Field name="age" component={MyField} type="number" 
-                    validate={isNumber} label="Edad" parse={toNumber} normalize={onlyGrow}></Field>
-                    
-                    <CustomersActions>
-                        <button type="submit" disabled={pristine || submitting}>Aceptar</button>
-                        <button type="button"  disabled={submitting} onClick={onBack}>Cancelar</button>
-                    </CustomersActions>
-                    {/* pristine es una propiedad de react routing para saber si se cambio el formulario */}
-                    <Prompt when={!pristine && !submitSucceeded}
-                    message="Se perderán los datos si continua"></Prompt>
-            </form>
+            <label htmlFor={name}>{label} :</label>
+            <input {...input} 
+            type={!type ? "text":type}
+            ref={withFocus && (txt => this.txt=txt)}></input>
+            {
+                meta.touched && meta.error && <span>{meta.error}</span>
+            }
         </div>
     );
-};
+
+    render(){
+        const {handleSubmit,submitting,onBack,pristine,submitSucceeded,withFocus} =this.props;
+        return (
+            <div>
+                <h2>Edicion del cliente</h2>
+                <form onSubmit={handleSubmit}>
+                        <Field withFocus
+                            name="name" component={this.renderField} type="text"
+                                    label="Nombre" parse={toUpper} format={toCamelCaseM}></Field>
+    
+                        <Field name="dni" component={this.renderField} type="text"
+                                validate={[isNumber]} label="Dni"></Field>
+    
+                        <Field name="age" component={this.renderField} type="number" 
+                        validate={isNumber} label="Edad" parse={toNumber} normalize={onlyGrow}></Field>
+                        
+                        <CustomersActions>
+                            <button type="submit" disabled={pristine || submitting}>Aceptar</button>
+                            <button type="button"  disabled={submitting} onClick={onBack}>Cancelar</button>
+                        </CustomersActions>
+                        {/* pristine es una propiedad de react routing para saber si se cambio el formulario */}
+                        <Prompt when={!pristine && !submitSucceeded}
+                        message="Se perderán los datos si continua"></Prompt>
+                </form>
+            </div>
+        );
+    }
+}
+
 
 CustomerEdit.propTypes = {
     name:PropTypes.string,
